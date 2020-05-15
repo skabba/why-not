d8a827c891b9730cf0139487297131b261453ac6
Enri Peters <enripeters@live.com>
2020-05-16 00:22:52 +0200

    Update btrfs.sh

--- bash/btrfs.sh
+++ bash/btrfs.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-### Create a 40GB BTRFS image file to mount later via a loopback device
+## Install packages
+## Create mount folder
+yum install -y btrfs-progs
+mkdir -p /mnt/btrfs /opt/btrfs
+
+### Create a 40GB image file to mount later via a loopback device
 ### Run in background with '&' at the end
-mkdir -p /opt/concourse/worker/volumes
-dd if=/dev/zero of=/opt/concourse/worker/btrfs.img bs=4k count=1000000 &
+dd if=/dev/zero of=/opt/btrfs/btrfs.img bs=4k count=1000000 &
 
 ### Check if the 'dd' command finished and mount BTRFS filesystem
 if ps -p $! >& /dev/null
 then
   wait $!
   modprobe btrfs
-  mkfs.btrfs /opt/concourse/worker/btrfs.img
-  losetup /dev/loop0 /opt/concourse/worker/btrfs.img
-  mount -t btrfs /dev/loop0 /opt/concourse/worker/volumes
+  mkfs.btrfs /opt/btrfs/btrfs.img
+  losetup /dev/loop0 /opt/btrfs/btrfs.img
+  mount -t btrfs /dev/loop0 /mnt/btrfs
 fi
